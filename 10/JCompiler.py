26d27e979f2dd446b5d92e7fed40590ac5e25191
dvirberlo <dvir.berlowitz@hotmail.com>
2021-10-06 22:24:25 +0300

    JCompiler- meaningless & syntax

---
+++ 10/JCompiler.py
@@ -0,0 +1,167 @@
+import os
+import sys
+# TODO: readable
+class JCompiler:
+    ext = '.jack'
+    # extP = '.vm'
+    extP = '.vm.xml'
+    version = '0.1'
+    name = 'JCompiler'
+
+    stringS = '"'
+    symbols = '{[]}().,;+-*/&!|<>='
+    keywords = 'class, constructor, function, method, field, static, var, int, char, boolean, void, true, false, null, this, let, do, if, else, while, return'.split(', ')
+      
+    def __init__(self):
+        self.insideComment = False
+        self.indentCounter = 0
+
+    def parse(self, filepath, writeCallback):
+        filename = os.path.basename(filepath).replace(self.ext, '')
+        with open(filepath) as file:
+            for line in file:
+                lineCode = self._parseMeaningless(line)
+                if line != '':
+                    # lineCode = self._parseSyntax( self._parseSyntax(lineCode))
+                    lineCode = self._parseSyntax(lineCode)
+                    writeCallback(lineCode)
+
+    def _parseMeaningless(self, line):
+      multyLineS = '/*'
+      multyLineE = '*/'
+      singleLineS = '//'
+      line = line.replace('\n', '').replace('\t', '')
+      if self.insideComment:
+        if multyLineE not in line:
+          line = ''
+        else:
+          line = line[line.index(multyLineE)+len(multyLineE):]
+      while multyLineS in line:
+        if multyLineE in line:
+          self.insideComment = False
+          line = line[:line.index(multyLineS)] + line[line.index(multyLineE)+len(multyLineE):]
+        else:
+          self.insideComment = True
+          line = line[:line.index(multyLineS)]
+      if singleLineS in line: line = line[:line.index('//')]
+      while line != '' and line[0] == ' ':
+        line = line[1:]
+      return line
+    
+    def _parseSyntax(self, line):
+      arr = []
+      while line != '':
+        if line[0] == ' ':
+          line = line[1:]
+        elif line[0] in self.symbols:
+          arr.append(['symbol', line[0]])
+          line = line[1:]
+        elif line[0] == self.stringS:
+          string = line.split(self.stringS)[1]
+          arr.append(['stringConstant', string])
+          line = line[len(string)+2:]
+        elif line.split(' ')[0][:self._firstSymbolIndex(line.split(' '))].isdigit():
+          integer = line.split(' ')[0][:self._firstSymbolIndex(line.split(' '))]
+          arr.append(['integerConstant', integer])
+          line = line[len(integer):]
+        elif line.split(' ')[0] in self.keywords:
+          keyword = line.split(' ')[0]
+          arr.append(['keyword', keyword])
+          line = line[len(keyword):]
+        else:
+          identifier = line.split(' ')[0]
+          identifier = identifier[:self._firstSymbolIndex(identifier)]
+          arr.append(['identifier', identifier])
+          line = line[len(identifier):]
+      xml = ''
+      for ele in arr:
+        xml += '<'+ele[0]+'> '+ele[1]+' </'+ele[0]+'>\n'
+      return xml
+    def _firstSymbolIndex(self, string):
+      index = len(string)
+      for symbol in self.symbols:
+        if symbol in string and string.index(symbol) < index:
+          index = string.index(symbol)
+      return index
+
+    def _parseTree(self, arr):
+      # TODO
+      pass
+    
+
+
+
+def main(argv):
+    trClass = JCompiler
+    help = 'Usage:\n' + '  python3 ' + trClass.name + '.py [ ...files| ...dirs] [ -r| --recursive]'
+    version = trClass.name + ' v' + trClass.version + ' for ' + trClass.ext +' files (nand2tetris.org)\n' + 'written by @dvirberlo'
+
+    recursiveWarn = 'WARNING: recursive mode is on.'
+    forceWarn = 'WARNING: force mode is on.'
+    removableArgs = {
+        '-r': recursiveWarn, '--recursive':recursiveWarn,
+        '-fsm': forceWarn, '--force-single-mode': forceWarn,
+        '-fmm': forceWarn, '--force-multy-mode': forceWarn,
+    }
+    args = argv[1:]
+    if len(args) == 0:
+        print(help + '\n\n' + version)
+    elif '-h' in args or '--help' in args:
+        print(help)
+    elif '-v' in args or '--version' in args:
+        print(version)
+    else:
+        paths = args
+        recursive = '-r' in args or '--recursive' in args
+        forceSingleMode = '-fsm' in args or '--force-single-mode' in args
+        forceMultyMode = '-fmm' in args or '--force-multy-mode' in args
+        for rmArg in removableArgs:
+            if rmArg in paths:
+                if removableArgs[rmArg]: print(removableArgs[rmArg])
+                paths.remove(rmArg)
+            while rmArg in paths:
+                paths.remove(rmArg)
+        
+        if recursive and len(paths) == 0:
+            paths = ['.']
+
+        for path in paths:
+            if os.path.isfile(path):
+                singleParse(trClass, path)
+            else:
+                for (dirPath, dirnames, filenames) in os.walk(path):
+                    extFilenames = [filename for filename in filenames if trClass.ext in filename]
+                    if len(extFilenames) == 0:
+                        print(trClass.name + ' skipped: ' + dirPath , flush=True)
+                    elif not forceMultyMode and (forceSingleMode or len(extFilenames) < 2):
+                        for filename in extFilenames:
+                            filePath = os.path.join(dirPath, filename)
+                            singleParse(trClass, filePath)
+                    else:
+                        multyParse(trClass, dirPath, extFilenames)
+
+def singleParse(trClass, filepath):
+    asmPath = filepath.replace(trClass.ext, trClass.extP)
+    with open(asmPath, 'w') as asmFile:
+        def singleParseCallback(asm):
+            asmFile.write(asm)
+        print(trClass.name + ' single-mode parse: ' + filepath + ' started... ', end='', flush=True)
+        trClass().parse(filepath, singleParseCallback)
+        print('done')
+
+def multyParse(trClass, dirPath, filenames):
+    asmPath = os.path.join(dirPath, os.path.basename(os.path.normpath(dirPath)) + trClass.extP)
+    with open(asmPath, 'w') as asmFile:
+        translator = trClass()
+        asmFile.write(translator.boot)
+        def multyParseCallback(asm):
+            asmFile.write(asm)
+        print(trClass.name + ' multy-mode parse: ' + dirPath + ' started... ', flush=True)
+        for filename in filenames:
+            filePath = os.path.join(dirPath, filename)
+            print('+  ' + filename + ' started... ', end='', flush=True)
+            translator.parse(filePath, multyParseCallback)
+            print('done', flush=True)
+        print('done', flush=True)
+
+if __name__ == '__main__': main(sys.argv)
