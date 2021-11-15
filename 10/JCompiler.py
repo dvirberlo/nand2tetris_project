b9fd0fb85ce12f6e1fe3229260d02853a7350b59
dvirberlo <dvir.berlowitz@hotmail.com>
2021-11-15 21:19:05 +0200

    temp upload (fixed code indentation)

--- 10/JCompiler.py
+++ 10/JCompiler.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import math
 # TODO: readable
 class JCompiler:
     ext = '.jack'
@@ -14,80 +15,186 @@ class JCompiler:
       
     def __init__(self):
         self.insideComment = False
-        self.indentCounter = 0
+        self.stack = []
+        self.lastToken = False
 
-    def parse(self, filepath, writeCallback):
+    def compile(self, filepath, writeCallback):
         filename = os.path.basename(filepath).replace(self.ext, '')
         with open(filepath) as file:
-          writeCallback('<tokens>\n')
           for line in file:
-              lineCode = self._parseMeaningless(line)
+              lineCode = self._removeMeaningless(line)
               if line != '':
-                  # lineCode = self._parseSyntax( self._parseSyntax(lineCode))
-                  lineCode = self._parseTokens(lineCode)
+                  lineCode = self._parse(self._tokenize(lineCode))
                   writeCallback(lineCode)
-          writeCallback('</tokens>\n')
 
-    def _parseMeaningless(self, line):
-      multyLineS = '/*'
-      multyLineE = '*/'
-      singleLineS = '//'
-      line = line.replace('\n', '').replace('\t', '')
-      if self.insideComment:
-        if multyLineE not in line:
-          line = ''
-        else:
-          line = line[line.index(multyLineE)+len(multyLineE):]
-      while multyLineS in line:
-        if multyLineE in line:
-          self.insideComment = False
-          line = line[:line.index(multyLineS)] + line[line.index(multyLineE)+len(multyLineE):]
-        else:
-          self.insideComment = True
-          line = line[:line.index(multyLineS)]
-      if singleLineS in line: line = line[:line.index('//')]
-      while line != '' and line[0] == ' ':
-        line = line[1:]
-      return line
+    def _removeMeaningless(self, line):
+        multyLineS = '/*'
+        multyLineE = '*/'
+        singleLineS = '//'
+        line = line.replace('\n', '').replace('\t', '')
+        if self.insideComment:
+            if multyLineE not in line:
+                line = ''
+            else:
+                line = line[line.index(multyLineE)+len(multyLineE):]
+        while multyLineS in line:
+            if multyLineE in line:
+                self.insideComment = False
+                line = line[:line.index(multyLineS)] + line[line.index(multyLineE)+len(multyLineE):]
+            else:
+                self.insideComment = True
+                line = line[:line.index(multyLineS)]
+        if singleLineS in line: line = line[:line.index('//')]
+        while line != '' and line[0] == ' ':
+            line = line[1:]
+        return line
     
-    def _parseTokens(self, line):
-      arr = []
-      while line != '':
-        if line[0] == ' ':
-          line = line[1:]
-        elif line[0] in self.symbols:
-          arr.append(['symbol', line[0]])
-          line = line[1:]
-        elif line[0] == self.stringS:
-          string = line.split(self.stringS)[1]
-          arr.append(['stringConstant', string])
-          line = line[len(string)+2:]
-        else:
-          nextword = line.split(' ')[0][:self._firstSymbolIndex(line.split(' ')[0])]
-          if nextword.isdigit():
-            arr.append(['integerConstant', nextword])
-            line = line[len(nextword):]
-          elif nextword in self.keywords:
-            arr.append(['keyword', nextword])
-            line = line[len(nextword):]
-          else:
-            arr.append(['identifier', nextword])
-            line = line[len(nextword):]
-      xml = ''
-      for ele in arr:
-        xml += '<'+ele[0]+'> '+ele[1].replace('<', '&lt;').replace('>', '&gt;')+' </'+ele[0]+'>\n'
-      return xml
+    def _tokenize(self, line):
+        tokens = []
+        while line != '':
+            if line[0] == ' ':
+                line = line[1:]
+            elif line[0] in self.symbols:
+                tokens.append(['symbol', line[0]])
+                line = line[1:]
+            elif line[0] == self.stringS:
+                string = line.split(self.stringS)[1]
+                tokens.append(['stringConstant', string])
+                line = line[len(string)+2:]
+            else:
+                nextword = line.split(' ')[0][:self._firstSymbolIndex(line.split(' ')[0])]
+                if nextword.isdigit():
+                    tokens.append(['integerConstant', nextword])
+                    line = line[len(nextword):]
+                elif nextword in self.keywords:
+                    tokens.append(['keyword', nextword])
+                    line = line[len(nextword):]
+                else:
+                    tokens.append(['identifier', nextword])
+                    line = line[len(nextword):]
+        return tokens
     def _firstSymbolIndex(self, string):
-      index = len(string)
-      for symbol in self.symbols:
-        if symbol in string and string.index(symbol) < index:
-          index = string.index(symbol)
-      return index
+        index = len(string)
+        for symbol in self.symbols:
+            if symbol in string and string.index(symbol) < index:
+                index = string.index(symbol)
+        return index
 
-    def _parseTree(self, arr):
-      # TODO
-      pass
-    
+    # TODO true, false, null, this
+    parseDict = {
+        'class': {
+            'self': ['class', '}', True],
+            'children': []
+        },
+        'constructor': {
+            'self': ['subroutineDec', '}', True],
+            'children': [
+                ['parameterList', '()', False],
+                ['subroutineBody', '{}', True],
+            ]
+        },
+        'function': {
+            'self': ['subroutineDec', '}', True],
+            'children': [
+                ['parameterList', '()', False],
+                ['subroutineBody', '{}', True],
+            ]
+        },
+        'method': {
+            'self': ['subroutineDec', '}', True],
+            'children': [
+                ['parameterList', '()', False],
+                ['subroutineBody', '{}', True],
+            ]
+        },
+        'field': {
+            'self': ['classVarDec', ';', True],
+            'children': []
+        },
+        'static': {
+            'self': ['classVarDec', ';', True],
+            'children': []
+        },
+        'var': {
+            'self': ['varDec', ';', True],
+            'children': []
+        },
+        'let': {
+            'self': ['letStatement', ';', True],
+            'children': []
+        },
+        'do': {
+            'self': ['doStatement', ';', True],
+            'children': []
+        },
+        'if': {
+            'self': ['ifStatement', ';', True],
+            'children': [
+                ['statements', '{}', True],
+            ]
+        },
+        'else': {
+            'self': ['', ';', True],
+            'children': [
+                ['statements', '{}', False],
+            ]
+        },
+        'while': {
+            'self': ['whileStatement', '}', True],
+            'children': [
+                ['statements', '{}', False],
+            ]
+        },
+        'return': {
+            'self': ['returnStatement', ';', True],
+            'children': []
+        },
+    }
+
+    def _parse(self, tokens):
+        xml = ''
+        for token in tokens:
+            preToken, postToken = '', ''
+            [cls, val] = token
+            if len(self.stack) > 0:
+                [key, name, counter] = self.stack[-1]
+                if val == self.parseDict[key]['self'][1]:
+                    # statement end
+                    self.stack.pop()
+                    write = self._xml(name, True) if name else ''
+                    if self.parseDict[key]['self'][2]: postToken = write
+                    else: preToken = write
+                elif len(self.parseDict[key]['children']) > 0 and val == self.parseDict[key]['children'][math.floor(counter/2)][1][counter%2]:
+                    # statement child start/end
+                    close = counter%2 == 1
+                    [childName, childG, before] = self.parseDict[key]['children'][math.floor(counter/2)]
+                    self.stack[-1] = [key, name, counter+1]
+                    write = self._xml(childName, close) if childName else ''
+                    if close: before = not before
+                    if before: preToken = write
+                    else: postToken = write
+            if val in self.parseDict.keys():
+                # statement start
+                [name, grammer, before] = self.parseDict[val]['self']
+                self.stack.append([val, name, 0])
+                write = self._xml(name) if name else ''
+                if before: preToken = write
+                else: postToken = write
+            else:
+                # TODO expression/List -> )]};
+                pass
+            xml += preToken + self._xml(token) + postToken
+            self.lastToken = token
+        return xml
+
+    def _xml(self, obj, close=False):
+        indent = len(self.stack) * '  '
+        if (type(obj) == str):
+            return indent + ('</' if close else '<') + self._xmlLang(obj) + ">\n"
+        else:
+            return indent + '<'+obj[0]+'> ' +self._xmlLang(obj[1]) +' </'+obj[0]+'>\n'
+    def _xmlLang(self, str):
+        return str.replace('<', '&lt;').replace('>', '&gt;')
 
 
 
@@ -146,7 +253,7 @@ def singleParse(trClass, filepath):
         def singleParseCallback(asm):
             asmFile.write(asm)
         print(trClass.name + ' single-mode parse: ' + filepath + ' started... ', end='', flush=True)
-        trClass().parse(filepath, singleParseCallback)
+        trClass().compile(filepath, singleParseCallback)
         print('done')
 
 def multyParse(trClass, dirPath, filenames):
@@ -160,7 +267,7 @@ def multyParse(trClass, dirPath, filenames):
         for filename in filenames:
             filePath = os.path.join(dirPath, filename)
             print('+  ' + filename + ' started... ', end='', flush=True)
-            translator.parse(filePath, multyParseCallback)
+            translator.compile(filePath, multyParseCallback)
             print('done', flush=True)
         print('done', flush=True)
 
