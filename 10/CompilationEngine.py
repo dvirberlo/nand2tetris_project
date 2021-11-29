90b80bf409bae2b9fbc2ed7edaa219ac5b3d8357
dvirberlo <dvir.berlowitz@hotmail.com>
2021-11-29 17:08:16 +0200

    moved writing action to CompilationEngine

--- 10/CompilationEngine.py
+++ 10/CompilationEngine.py
@@ -1,6 +1,52 @@
+from JackTokenizer import Token
+
 class CompilationEngine:
-    def __init__(self) -> None:
+    def __init__(self, file) -> None:
+        self.file = file
+
+    def write(self, string) -> None:
+        assert type(string) == str, 'writes to file strings only'
+        self.file.write(string)
+    
+    def writeTokenXml(self, token, indentC) -> None:
+        assert type(token) == Token, 'token is not a Token'
+        indentS = '  '
+        self.write(indentC * indentS + '<'+token.kind+'> ' +self._xmlLang(token.string) +' </'+token.kind+'>\n')
+    
+    def _xmlLang(self, str) -> str:
+        return str.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;').replace('\'', '&apos;').replace('"', '&quot;')
+    
+    
+    def Class(self) -> None:
+        pass
+    
+    def ClassVarDec(self) -> None:
+        pass
+    
+    def SubroutineDec(self) -> None:
+        pass
+    
+    def ParameterList(self) -> None:
+        pass
+    
+    def SubroutineBody(self) -> None:
+        pass
+    
+    def VarDec(self) -> None:
+        pass
+    
+    def Statements(self) -> None:
         pass
 
-    def subroutineDec(self, token) -> str:
+    def StatementsTypes(self, keyWord) -> None:
+        assert keyWord in ['let', 'do', 'if', 'while', 'return']
+        pass
+    
+    def ExpressionList(self) -> None:
+        pass
+    
+    def Expression(self) -> None:
+        pass
+    
+    def Term(self) -> None:
         pass
